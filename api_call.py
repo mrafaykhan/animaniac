823bed13e68a858c0bbd1f45633e6d011b417fa8
Rafay Khan <mrafay.khan@outlook.com>
2022-12-11 01:02:17 -0600

    Finished the API and dif the set

--- api_call.py
+++ api_call.py
@@ -2,10 +2,14 @@ import requests
 # Here we define our query as a multi-line string
 query = '''
 query ($name: String) { # Define which variables will be used in the query (id)
-  MediaList (userName: $name) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
-    media{
-      title{
-        romaji
+  MediaListCollection (userName: $name, type: ANIME, status: COMPLETED, sort:MEDIA_TITLE_ENGLISH) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+    lists{
+      entries{
+        media{
+          title{
+            english
+          }
+        }
       }
     }
   }
@@ -22,4 +26,31 @@ url = 'https://graphql.anilist.co'
 # Make the HTTP Api request
 response = requests.post(url, json={'query': query, 'variables': variables})
 
-print(response.json())
+user1 = "rafay"
+user2 = "wasaucy"
+user3 = "rayaanb"
+
+response1 = requests.post(url, json={'query': query, 'variables': {'name':user1}})
+response2 = requests.post(url, json={'query': query, 'variables': {'name':user2}})
+response3 = requests.post(url, json={'query': query, 'variables': {'name':user3}})
+
+# prettyify data
+response1 = response1.json()['data']['MediaListCollection']['lists'][0]['entries']
+response2 = response2.json()['data']['MediaListCollection']['lists'][0]['entries']
+response3 = response3.json()['data']['MediaListCollection']['lists'][0]['entries']
+
+# Extract from the responses
+user1_anime = set()
+user2_3_anime = set()
+for anime in response1:
+  user1_anime.add(anime['media']['title']['english'])
+for anime in response2:
+  user2_3_anime.add(anime['media']['title']['english'])
+for anime in response3:
+  user2_3_anime.add(anime['media']['title']['english'])
+
+print(user1.upper(),"ANIME: \n",user1_anime,'\n\n\n')
+print(user2.upper(),"AND",user3.upper(),"ANIME: \n",user2_3_anime,'\n\n\n')
+
+anime_dif = user1_anime.difference(user2_3_anime)
+print("YOU NEED TO WATCH THESE ANIME TO HAVE RAFAY WATCH ONE PIECE (",len(anime_dif),") :\n",anime_dif)
